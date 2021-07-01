08220f44e5b8c68edfe9077f025b8fbf5ad05500
KingJulianDev <kingurianjulian@gmail.com>
2021-07-01 21:58:10 +0300

    'commit'

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-// LECTURES
+////////////////////////////////////////////////////////////
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -67,4 +67,14 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+
